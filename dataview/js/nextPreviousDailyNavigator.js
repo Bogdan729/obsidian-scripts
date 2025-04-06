72a4b2039cb21d12bb525d35809251c262daf8b0
Bogdan <kozukhov.bogdan@gmail.com>
2025-04-06 17:44:42 +0300

    Добавить содержимое для шаблонов

---
+++ dataview/js/nextPreviousDailyNavigator.js
@@ -0,0 +1,16 @@
+// Отображение предыдущей и следующей заметки в текущей daily
+
+const currentNote = dv.current().file.name;
+
+if (currentNote != "bdn-daily") {
+	const pn = dv.pages('"personal space/bogdan/daily"')
+		.sort(n => n.file.ctime.toMillis(), "asc")
+	
+	const currentIndex = pn.findIndex(n => n.file.name === currentNote);
+	
+	if (pn.length > currentIndex + 1) {
+		dv.paragraph(`${pn[currentIndex-1].file.link} || ${pn[currentIndex+1].file.link}`);
+	} else {
+		dv.paragraph(`${pn[currentIndex-1].file.link}`);
+	}
+}
